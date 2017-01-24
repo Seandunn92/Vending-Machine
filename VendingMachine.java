e3952dbd5d861666090c87230df91324fc97c869
Seandunn92 <seandunn92@gmail.com>
2017-01-23 23:47:54 -0500

    Add files via upload

--- VendingMachine.java
+++ VendingMachine.java
@@ -3,26 +3,56 @@
 public class VendingMachine {
 	//A nickel weighs 5 grams exactly
 	public static final double NICKELWEIGHT = 5.0;
+	//A nickel measures 22.21 milimeters diameter;
+	public static final double NICKELSIZE = 21.21;
 	// A dime weighs 2.68 grams;
 	public static final double DIMEWEIGHT = 2.268;
+	//A dime measures 17.91 millimeters diameter
+	public static final double DIMESIZE = 17.91;
 	//A quarter weighs 5.670 grams;
 	public static final double QUARTERWEIGHT = 5.670;
+	//A quarter's diameter is 24.26 millimeters;
+	public static final double QUARTERSIZE = 24.26;
+	
+	//Chips cost $0.50
 	public static final double CHIPSCOST = 0.50;
+	//Sodas cost $1.00
 	public static final double SODACOST = 1.00;
+	//Candy costs $0.65
 	public static final double CANDYCOST = 0.65;
 
 	private double total;
 	private int Chips;
 	private int Sodas;
 	private int Candy;
+	private int Quarters;
+	private int Dimes;
+	private int Nickels;
+	
+	public void setTotal(double total) {this.total=total;}
+	public double getTotal() {return total;}
+	public void setQuarters(int Quarters){this.Quarters=Quarters;}
+	public int getQuarters(){return Quarters;}
+	public void setDimes(int Dimes){this.Dimes=Dimes;}
+	public int getDimes(){return Dimes;}
+	public void setNickels(int Nickels){this.Nickels=Nickels;}
+	public int getNickels(){return Nickels;}
+	
 	//A method to convert given weight to corresponding Value
-	private double convertWeight(double d){
-		if (d==NICKELWEIGHT)
+		//if the method has a corresponding coinvalue will also increase the number of that coin in the machine
+	private double convertWeightandSize(double weight, double size){
+		if (weight==NICKELWEIGHT & size==NICKELSIZE){
+			Nickels++;
 			return 0.05;
-		else if (d==DIMEWEIGHT)
+		}
+		else if (weight==DIMEWEIGHT & size==DIMESIZE){
+			Dimes++;
 			return 0.10;
-		else if (d==QUARTERWEIGHT)
+		}
+		else if (weight==QUARTERWEIGHT & size==QUARTERSIZE){
+			Quarters++;
 			return 0.25;
+		}
 		else 
 			return 0.00;
 	}
@@ -32,6 +62,10 @@ public class VendingMachine {
 		this.Chips=Chips;
 		this.Sodas = Sodas;
 		this.Candy = Candy;
+		
+		Quarters=10;
+		Nickels=10;
+		Dimes=10;
 
 	}
 
@@ -40,20 +74,51 @@ public class VendingMachine {
 		Chips = 10;
 		Sodas = 10;
 		Candy = 10;
+		Quarters=10; 
+		Dimes=10;
+		Nickels=10;
 	}
 
-	public double insert(double CoinWeight) {
+
+	public double insert(double CoinWeight, double CoinSize) {
 		// TODO Auto-generated method stub
-		total+= convertWeight(CoinWeight);
+		total+= convertWeightandSize(CoinWeight, CoinSize);
 
 		return total;
 	}
 
+	//This function makes change from the remaining total either by Customer request or by Transaction
+	public double makeChange(double total){
+		//start taking away as many Quarters as possible
+		while (total/0.25>=1){
+			if (Quarters<=0)
+			break;
+			Quarters--;
+			total-=0.25;
+		}
+		while (total/0.10>=1){
+			if (Dimes<=0)
+			break;
+			Dimes--;
+			total-=0.10;
+		}
+		while (total/0.05>=1){
+			if (Nickels <=0)
+			break;
+			Nickels--;
+			total-=0.05;
+		}
+		
+		return total;
+		
+	}
+	
 	public String purchase(String ITEMCHOICE) {
 		// TODO Auto-generated method stub
 		if (ITEMCHOICE == "Chips"){
 			if (total>=CHIPSCOST){
 				Chips--;
+				makeChange(total-CHIPSCOST);
 				return "THANK YOU";
 			}
 			//trick from onlineto make 2 decimal places
@@ -62,6 +127,7 @@ public class VendingMachine {
 		if (ITEMCHOICE == "Candy"){
 			if (total>=CANDYCOST){
 				Candy--;
+				makeChange(total-CANDYCOST);
 				return "THANK YOU";
 			}
 			return "Price " + String.format("%.2f", CANDYCOST);
@@ -69,7 +135,9 @@ public class VendingMachine {
 		
 		if (ITEMCHOICE == "Soda"){
 			if (total>=SODACOST){
+				
 				Sodas--;
+				makeChange(total-SODACOST);
 				return "THANK YOU";
 			}
 			return "Price " + String.format("%.2f", SODACOST);
@@ -78,5 +146,7 @@ public class VendingMachine {
 		return null;
 
 	}
+
+	
 
 }
--- VendingMachineTest.java
+++ VendingMachineTest.java
@@ -10,21 +10,21 @@ public class VendingMachineTest {
 	@Test
 	public void whenEmptyVendingMachineIsPassedAQuarterItReturnsAQuarter() {
 		VendingMachine vender = new VendingMachine();
-		assertEquals(0.25, vender.insert(vender.QUARTERWEIGHT), 0.01);
+		assertEquals(0.25, vender.insert(vender.QUARTERWEIGHT, vender.QUARTERSIZE), 0.01);
 	}
 	@Test
 	public void whenEmptyVendingMachineIsPassedANickelItReturnsANickel(){
 		VendingMachine vender = new VendingMachine();
-		assertEquals(0.05,vender.insert(vender.NICKELWEIGHT), 0.01);
+		assertEquals(0.05,vender.insert(vender.NICKELWEIGHT, vender.NICKELSIZE), 0.01);
 	}
 	public void whenEmptyVendingMachineIsPassedADimeItReturnsADime(){
 		VendingMachine vender = new VendingMachine();
-		assertEquals(0.10,vender.insert(vender.DIMEWEIGHT), 0.01);
+		assertEquals(0.10,vender.insert(vender.DIMEWEIGHT, vender.DIMESIZE), 0.01);
 	}
 	@Test
 	public void whenVendingMachineWithADollarIsGivenAQuarterItReturnsDollarAndQuarter(){
 		VendingMachine vender = new VendingMachine(1.00, 10, 10, 10);
-		assertEquals(1.25, vender.insert(vender.QUARTERWEIGHT), 0.01);
+		assertEquals(1.25, vender.insert(vender.QUARTERWEIGHT, vender.QUARTERSIZE), 0.01);
 	}
 	@Test
 	public void whenEnoughMoneyForChipsChipsAreDispensedWhenSelected(){
@@ -56,6 +56,62 @@ public class VendingMachineTest {
 	public void whenNotEnoughMoneyForSodaSodaAreNotDispensedWhenSelected(){
 		VendingMachine vender = new VendingMachine(0.35, 10, 10, 10);
 		assertEquals("Price 1.00", vender.purchase("Soda"));
+	}
+	/* This test does not account for the edge case where the customer inserted money without verifying the machine had change
+	@Test
+	public void VerifyEmptyVendingMachinewhenPurchaseMade(){
+		VendingMachine vender = new VendingMachine(2.00, 10, 10, 10);
+		vender.purchase("Chips");
+		assertEquals(0.00, vender.getTotal(), 0.01);
+		vender.setTotal(2.00);
+		
+		vender.purchase("Candy");
+		assertEquals(0.00, vender.getTotal(), 0.01);
+		vender.setTotal(2.00);
+		
+		vender.purchase("Soda");
+		assertEquals(0.00, vender.getTotal(), 0.01);
+	} 
+	*/
+	
+	@Test
+	public void VerifyMakeChange(){
+		VendingMachine vender = new VendingMachine();
+		
+		//set Quarters to 10 and test to make sure we stop when no quarters are left since machine should not be able to provide $6.00 worth of quarters
+		vender.setQuarters(10);
+		vender.makeChange(6.00);
+		assertTrue(vender.getQuarters()>=0);
+		
+		
+		//setQuarters to 10 and test making change for 1 dollar, we should have 6 quarters left
+		vender.setQuarters(10);
+		vender.makeChange(1.00);
+		assertEquals(6, vender.getQuarters());
+		
+		// Now we test for dimes, start by setting quarters equal to 0
+		vender.setQuarters(0);
+		//if we have 10 dimes and need to give 1.10 back, need to make sure vender stops giving dimes when dimes gets to 0
+		vender.setDimes(10);
+		vender.makeChange(1.90);
+		assertTrue(vender.getDimes()>=0);
+		
+		//set dimes to 15 and make change for 1 dollar, should have 5 dimes left (Quarters is still set to 0)
+		vender.setDimes(15);
+		vender.makeChange(1.00);
+		assertEquals(5, vender.getDimes());
+		
+		//Now we 0 out dimes and make sure Nickels is Working
+		vender.setDimes(0);
+		vender.setNickels(10);
+		vender.makeChange(0.55);
+		assertTrue(vender.getNickels()>=0);
+		
+		//Test for if when we have 15 nickels, 0 dimes, and 0 quarters, 50 cents needs to be refunded, we should have 5 nickels left
+		vender.setNickels(15);
+		vender.makeChange(0.50);
+		System.out.println(vender.getNickels());
+		assertEquals(5, vender.getNickels());
 	}
 	
 	
